9bcdf2dc78eee13dd2d01edff3751bcde1183e9a
mimas <univ.t1c2ha3@gmail.com>
2022-08-17 23:48:29 +0900

    タイマー関数およびMMのフォルダの作成

---
+++ Marathon_Match_Tools/__template.cpp
@@ -0,0 +1,13 @@
+# include "bits/stdc++.h"
+using namespace std;
+using LL = long long;
+# define ALL(qpqpq)           (qpqpq).begin(),(qpqpq).end()
+# define UNIQUE(wpwpw)        sort(ALL((wpwpw)));(wpwpw).erase(unique(ALL((wpwpw))),(wpwpw).end())
+# define REP(i,upupu)         for(int i = 0, i##_len = (upupu);(i) < (i##_len);(i)++)
+# define INIT                 std::ios::sync_with_stdio(false);std::cin.tie(0)
+
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+const double TIME_LIMIT = 15000.0;
