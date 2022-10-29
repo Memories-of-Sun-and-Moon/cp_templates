e83a7a929837b4e598e21006e1728aec8c96ea5f
mimas <univ.t1c2ha3@gmail.com>
2022-10-29 18:12:12 +0900

    dinicにした

--- Graph_Algorithm/max_flow.cpp
+++ Graph_Algorithm/max_flow.cpp
@@ -2,7 +2,7 @@
 # include <bits/stdc++.h>
 using namespace std;
 
-//Ford-Fulkerson algorithm O((max_flow)|E|)
+//dinic algorithm O(|E||v|^2)よりも非常に高速に動作 
 template<typename T>struct max_flow {
 
     struct edge{
@@ -13,11 +13,13 @@ template<typename T>struct max_flow {
     
     int v;
     vector<vector<edge>> G;
-    vector<bool> used;
+    vector<int> level; // sからの距離
+    vector<int> iter; // どこまで調べ終わったか
 
     max_flow(int _v) : v(_v) {
         G.resize(v);
-        used.resize(v, false);
+        level.resize(v);
+        iter.resize(v);
     }
 
     void add_edge(int from, int to, T cap){
@@ -25,12 +27,31 @@ template<typename T>struct max_flow {
         G[to].push_back((edge){from, 0, (T)(G[from].size() - 1)});
     }
 
+    // s 空の最短距離を計算
+    void bfs(int s){
+        for(int i = 0;i < v;i++)level[i] = -1;
+        queue<int> que;
+        level[s] = 0;
+        que.push(s);
+        while(!que.empty()){
+            int v = que.front();
+            que.pop();
+            for(int i = 0;i < (int)G[v].size();i++){
+                edge &e = G[v][i];
+                if(e.cap > 0 && level[e.to] < 0){
+                    level[e.to] = level[v] + 1;
+                    que.push(e.to);
+                }
+            }
+        }
+    }
+
+    //増加パスをDFSで探す
     T dfs(int v, int t, T f){
         if(v == t)return f;
-        used[v] = true;
-        for(int i = 0;i < (int)G[v].size();i++){
+        for(int &i = iter[v];i < (int)G[v].size();i++){
             edge &e = G[v][i];
-            if(!used[e.to] && e.cap > 0){
+            if(e.cap > 0 && level[v] < level[e.to]){
                 T d = dfs(e.to, t, min(f, e.cap));
                 if(d > 0){
                     e.cap -= d;
@@ -45,10 +66,13 @@ template<typename T>struct max_flow {
     T calc(int s, int t){
         T flow = 0;
         for(;;){
-            for(int i = 0;i < v;i++)used[i] = false;
-            int f = dfs(s, t, INF<T>());
-            if(f == 0)return flow;
-            flow += f;
+            bfs(s);
+            if(level[t] < 0)return flow;
+            for(int i = 0;i < v;i++)iter[i] = 0;
+            int f;
+            while((f = dfs(s, t, INF<T>())) > 0) {
+                flow += f;
+            }
         }
     }
 };
