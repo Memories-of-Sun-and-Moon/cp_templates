599fad423f8647077f9d4e21dbd8c8eea6ba206b
mimas <univ.t1c2ha3@gmail.com>
2022-10-29 16:51:29 +0900

    最大流の追加

---
+++ Graph_Algorithm/max_flow.cpp
@@ -0,0 +1,68 @@
+# define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/6/GRL_6_A"
+# include <bits/stdc++.h>
+using namespace std;
+
+//Ford-Fulkerson algorithm O(V|E|)
+template<typename T>struct max_flow {
+
+    struct edge{
+        int to;
+        T cap;
+        T rev;
+    };
+    
+    int v;
+    vector<vector<edge>> G;
+    vector<bool> used;
+
+    max_flow(int _v) : v(_v) {
+        G.resize(v);
+        used.resize(v, false);
+    }
+
+    void add_edge(int from, int to, T cap){
+        G[from].push_back((edge){to, cap, (T)G[to].size()});
+        G[to].push_back((edge){from, 0, (T)(G[from].size() - 1)});
+    }
+
+    T dfs(int v, int t, T f){
+        if(v == t)return f;
+        used[v] = true;
+        for(int i = 0;i < (int)G[v].size();i++){
+            edge &e = G[v][i];
+            if(!used[e.to] && e.cap > 0){
+                T d = dfs(e.to, t, min(f, e.cap));
+                if(d > 0){
+                    e.cap -= d;
+                    G[e.to][e.rev].cap += d;
+                    return d;
+                }
+            }
+        }
+        return 0;
+    }
+
+    T calc(int s, int t){
+        T flow = 0;
+        for(;;){
+            for(int i = 0;i < v;i++)used[i] = false;
+            int f = dfs(s, t, INF<T>());
+            if(f == 0)return flow;
+            flow += f;
+        }
+    }
+};
+
+int v, e;
+
+int main(){
+    cin >> v >> e;
+    max_flow<int> mf(v);
+
+    for(int i = 0;i < e;i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        mf.add_edge(a, b, c);
+    }
+    cout << mf.calc(0, v - 1) << endl;
+}
