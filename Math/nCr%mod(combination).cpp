22afa57d422c96daca65e555c83b9ea45ade2006
mimas <univ.t1c2ha3@gmail.com>
2023-01-22 00:21:59 +0900

    mintを反映

--- Math/nCr%mod(combination).cpp
+++ Math/nCr%mod(combination).cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 # include "modint.cpp"
 
-using modi = modint<998244353>;
-//using modi = modint<1000000007>;
+using mint = modint<998244353>;
+//using mint = modint<1000000007>;
 
 constexpr int max_combination = 1010101;
 
-modi fact[max_combination];
+mint fact[max_combination];
 
 void combination_init(){
     fact[0] = 1;
@@ -18,12 +18,12 @@ void combination_init(){
     }
 }
 
-modi nCr(int n, int r){
+mint nCr(int n, int r){
     if(fact[0].a == 0)combination_init();
 
     if(r < 0 || r > n)return 0;
 
-    modi ret = fact[n];
+    mint ret = fact[n];
     ret /= fact[r];
     ret /= fact[n - r];
 
