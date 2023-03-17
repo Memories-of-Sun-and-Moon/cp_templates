86c0b9efe7ee399acfb6e751414a016545ad377c
mimas <univ.t1c2ha3@gmail.com>
2023-03-17 23:45:23 +0900

    euler_phi_table

---
+++ Math/euler_phi_table.cpp
@@ -0,0 +1,19 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+//O(n log log n)
+// [1, n] の phi(x) を求める
+vector<int> euler_phi_table(int n){
+    vector<int> phi(n + 1);
+    for(int i = 0;i <= n;i++){
+        phi[i] = i;
+    }
+    for(int i = 2;i <= n;i++){
+        if(phi[i] == i){
+            for(int j = i;j <= n;j += i){
+                phi[j] = phi[j] / i * (i - 1);
+            }
+        }
+    }
+    return phi;
+}
