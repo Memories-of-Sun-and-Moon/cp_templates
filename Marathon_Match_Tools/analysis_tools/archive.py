3c7447b32a5a6ca321f9c9c30941e5a104da876d
MMRZ <104312078+Memories-of-Sun-and-Moon@users.noreply.github.com>
2023-11-10 22:13:20 +0900

    score の保存

--- Marathon_Match_Tools/analysis_tools/archive.py
+++ Marathon_Match_Tools/analysis_tools/archive.py
@@ -14,4 +14,5 @@ def main(name: str):
     
     subprocess.run(['mv', '-v', 'out', f'{name}' + '/out'])
     subprocess.run(['mv', '-v', f'{name}', 'archive/' + f'{name}'])
+    subprocess.run(['mv', 'score.txt', 'archive/' + f'{name}' + '/score.txt'])
     subprocess.run(['cp', 'AHCXXX.cpp', 'archive/' + f'{name}' + '/source.cpp'])
