c61937761880c45b39f00704b7e3b2b649e912ad
ouchiPC <univ.t1c2ha3@gmail.com>
2024-03-13 22:29:36 +0900

    tah change

--- Math/power.cpp
+++ Math/power.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 long long power(long long n, long long k, long long m) {
-    n %= m;
+	n %= m;
 	long long ret = 1;
-    while(k > 0){
-        if(k & 1)ret = ret * n % m;
-        n = n * n % m;
-        k >>= 1;
-    }
-    return ret % m;
+	while(k > 0){
+		if(k & 1)ret = ret * n % m;
+		n = n * n % m;
+		k >>= 1;
+	}
+	return ret % m;
 }
 
 long long m, n;
 const long long mod = 1000000000 + 7;
 
 int main(){
-    cin >> m >> n;
-    cout << power(m, n, mod) << endl;
+	cin >> m >> n;
+	cout << power(m, n, mod) << endl;
 }
