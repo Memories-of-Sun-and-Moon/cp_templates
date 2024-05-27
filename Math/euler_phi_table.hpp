e69d7d73b2ae853711ca7a8e3cb5a19c4490210f
ouchiPC <univ.t1c2ha3@gmail.com>
2024-05-28 05:43:26 +0900

    rename & change indent

---
+++ Math/euler_phi_table.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> euler_phi_table(int n){
+	vector<int> phi(n + 1);
+	for(int i = 0;i <= n;i++){
+		phi[i] = i;
+	}
+	for(int i = 2;i <= n;i++){
+		if(phi[i] == i){
+			for(int j = i;j <= n;j += i){
+				phi[j] = phi[j] / i * (i - 1);
+			}
+		}
+	}
+	return phi;
+}
