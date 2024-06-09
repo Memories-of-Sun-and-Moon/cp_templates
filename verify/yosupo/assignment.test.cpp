b553be91f8bf1e84ed3b22e91d8e966e0107684e
ouchiPC <univ.t1c2ha3@gmail.com>
2024-06-09 16:27:06 +0900

    fix

---
+++ verify/yosupo/assignment.test.cpp
@@ -0,0 +1,19 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/assignment"
+
+#include "./../../template/template.hpp"
+#include "./../../graph/hungarian.hpp"
+
+using namespace mmrz;
+
+void mmrz::solve(){
+	int n;
+	cin >> n;
+	vector a(n, vector(n, 0LL));
+	rep(i, n)rep(j, n)cin >> a[i][j];
+
+	vector<int> h = hungarian<true, ll>(a);
+	ll x = 0;
+	rep(i, n)x += a[i][h[i]];
+	cout << x << endl;
+	rep(i, n)cout << h[i] << " \n"[i == n-1];
+}
