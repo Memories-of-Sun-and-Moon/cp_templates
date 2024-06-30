95cf079e7ed2a1517b14572541ea0c56cf723b21
ouchiPC <univ.t1c2ha3@gmail.com>
2024-07-01 00:39:11 +0900

    primality_test

---
+++ verify/yosupo/primality_test.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/primality_test"
+
+#include "./../../template/template.hpp"
+#include "./../../math/is_prime.hpp"
+
+using namespace mmrz;
+
+void mmrz::solve(){
+    int t;
+    cin >> t;
+    while(t--){
+        ll a;
+        cin >> a;
+        cout << (is_prime(a) ? "Yes" : "No") << endl;
+    }
+}
