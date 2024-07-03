d9051eddee4b35292982c64c204f775ca352313b
Enceladus <univ.t1c2ha3@gmail.com>
2024-07-03 18:57:49 +0900

    add : mo's algo

---
+++ data_structure/mo.hpp
@@ -0,0 +1,40 @@
+class Mo {
+    vector<pair<int, int>> lr;
+public:
+    Mo() = default;
+    Mo(const vector<pair<int, int>> &_lr) : lr(_lr) {}
+
+    template<typename AL, typename AR, typename EL, typename ER, typename F>
+    void calc(const AL &add_left, const AR &add_right, const EL &erase_left, const ER& erase_right, const F &f, int _n = -1, int _B = -1){
+        int n = (_n == -1 ? ranges::max(lr, {}, &pair<int, int>::second).second : _n);
+        int q = (int)lr.size();
+        int B = (_B == -1 ? max(1, n/int(sqrt(q))) : _B);
+
+        vector<int> index(q);
+        iota(index.begin(), index.end(), 0);
+        sort(index.begin(), index.end(), [&](int i, int j){
+            const auto &[l_i, r_i] = lr[i];
+            const auto &[l_j, r_j] = lr[j];
+            const int B_i = l_i / B, B_j = l_j / B;
+            return (B_i != B_j ? (B_i < B_j) : (r_i < r_j));
+        });
+
+        int l = 0, r = 0;
+        for(int idx : index){
+            const auto &[L, R] = lr[idx];
+
+            while(L < l)add_left(--l);
+            while(l < L)erase_left(l++);
+            
+            while(r < R)add_right(r++);
+            while(R < r)erase_right(--r);
+
+            f(idx);
+        }
+    }
+
+    template<typename A, typename E, typename F>
+    void calc(const A &add, const E &erase, const F &f){
+        calc(add, add, erase, erase, f);
+    }
+};
---
+++ verify/yosupo/static_range_frequency.test.cpp
@@ -0,0 +1,64 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/static_range_frequency"
+
+#include "./../../template/template.hpp"
+#include "./../../data_structure/mo.hpp"
+
+using namespace mmrz;
+
+void mmrz::solve(){
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for(auto &x : a)cin >> x;
+    if(q == 0){
+        return;
+    }
+    if(n == 0){
+        while(q--){
+            int l, r, x;
+            cin >> l >> r >> x;
+            cout << 0 << endl;
+        }
+        return;
+    }
+    vector<pair<int, int>> lr(q);
+    vector<int> x(q);
+    rep(i, q){
+        cin >> lr[i].first >> lr[i].second >> x[i];
+    }
+
+    {
+        vector<int> v;
+        for(auto &e : a)v.pb(e);
+        for(auto &e : x)v.pb(e);
+        sort(all(v));
+        UNIQUE(v);
+        map<int, int> cmp;
+        rep(i, len(v))cmp[v[i]] = i;
+        for(auto &e : a)e = cmp[e];
+        for(auto &e : x)e = cmp[e];
+    }
+
+    Mo mo(lr);
+
+    vector<int> cnt(n+q);
+
+    auto add = [&](int idx) -> void {
+        cnt[a[idx]]++;
+    };
+
+    auto erase = [&](int idx) -> void {
+        cnt[a[idx]]--;
+    };
+
+    vector<int> ans(q);
+    auto f = [&](int query_id) -> void {
+        ans[query_id] = cnt[x[query_id]];
+    };
+
+    mo.calc(add, erase, f);
+
+    rep(i, q){
+        cout << ans[i] << '\n';
+    }
+}
