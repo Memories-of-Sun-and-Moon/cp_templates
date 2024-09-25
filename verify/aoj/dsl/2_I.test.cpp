f576ba41ff72f7af0d48f9df4960c21192a63755
ouchiPC <univ.t1c2ha3@gmail.com>
2024-09-26 01:20:56 +0900

    遅延セグ木のテスト増幅

---
+++ verify/aoj/dsl/2_I.test.cpp
@@ -0,0 +1,44 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_F"
+
+#include "./../../../template/template.hpp"
+#include "./../../../data_structure/lazy_segment_tree.hpp"
+
+struct S{
+	ll val;
+	int size;
+};
+using F = ll;
+constexpr F ID = -10000;
+
+S op(S a, S b){ return {a.val+b.val, a.size+b.size}; }
+S e(){ return {0, 0}; }
+S mapping(F f, S x){
+	if(f != ID)x.val = f*x.size;
+	return x;
+}
+F composition(F f, F g){ return (f == ID ? g : f); }
+F id(){ return ID; }
+
+void mmrz::solve(){
+	int n, q;
+	cin >> n >> q;
+
+	vector<S> _v(n, {0, 1});
+	lazy_segment_tree<S, op, e, F, mapping, composition, id> seg(_v);
+
+	while(q--){
+		int op;
+		cin >> op;
+		if(op == 0){
+			int s, t, x;
+			cin >> s >> t >> x;
+			t++;
+			seg.apply(s, t, x);
+		}else{
+			int s, t;
+			cin >> s >> t;
+			t++;
+			cout << seg.fold(s, t).val << endl;
+		}
+	}
+}
