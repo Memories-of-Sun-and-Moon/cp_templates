11e7ad651a9348bff34a10c217437cca78ce6344
ouchiPC <univ.t1c2ha3@gmail.com>
2024-09-26 01:34:22 +0900

    bundle 時に cerr が無いかどうかを判定する

--- bundle.py
+++ bundle.py
@@ -6,6 +6,10 @@ file = sys.argv[1]
 with open(file, 'r') as file:
 	lines = file.readlines()
 
+for line in lines:
+	if "cerr" in line:
+		raise ValueError("プログラム中に cerr が含まれています。forループなどで cerr デバッグしたものを消さずに提出すると TLE となる可能性があります。")
+
 lines = [lines[0]] + ["#define debug(...) (static_cast<void>(0))\n"] + lines[3:]
 
 with open('z.cpp', 'w') as file:
