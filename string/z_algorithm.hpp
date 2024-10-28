dcfbac82fad55ff568cb18a91367a3194a8381a5
ouchiPC <univ.t1c2ha3@gmail.com>
2024-10-29 02:59:59 +0900

    space to tab

--- string/z_algorithm.hpp
+++ string/z_algorithm.hpp
@@ -1,24 +1,24 @@
 
 vector<int> z_algorithm(const string &s){
-    vector<int> z(s.size());
-    z[0] = (int)z.size();
-    int i = 1, j = 0;
-    while(i < (int)z.size()){
-        while(i+j < (int)s.size() && s[j] == s[i+j])j++;
-        z[i] = j;
-        
-        if(j == 0){
-            i++;
-            continue;
-        }
-        
-        int k = 1;
-        while(k < j && k + z[k] < j){
-            z[i+k] = z[k];
-            k++;
-        }
-        i += k;
-        j -= k;
-    }
-    return z;
+	vector<int> z(s.size());
+	z[0] = (int)z.size();
+	int i = 1, j = 0;
+	while(i < (int)z.size()){
+		while(i+j < (int)s.size() && s[j] == s[i+j])j++;
+		z[i] = j;
+		
+		if(j == 0){
+			i++;
+			continue;
+		}
+		
+		int k = 1;
+		while(k < j && k + z[k] < j){
+			z[i+k] = z[k];
+			k++;
+		}
+		i += k;
+		j -= k;
+	}
+	return z;
 }
