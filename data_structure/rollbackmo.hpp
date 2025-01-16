c5d35ea7a7c9a43f9660ee64d73828a81cf9101d
ouchiPC <univ.t1c2ha3@gmail.com>
2025-01-16 16:15:13 +0900

    rollback mo

---
+++ data_structure/rollbackmo.hpp
@@ -0,0 +1,55 @@
+
+class RollbackMo {
+	vector<pair<int, int>> lr;
+public:
+	RollbackMo() = default;
+	RollbackMo(const vector<pair<int, int>> &_lr) : lr(_lr) {}
+
+	template<typename AL, typename AR, typename RST, typename SNP, typename RB, typename F>
+	void calc(const AL &add_left, const AR &add_right, const RST &reset, const SNP &snapshot, const RB &rollback, const F &f,  int _n = -1, int _B = -1){
+		int n = (_n == -1 ? ranges::max(lr, {}, &pair<int, int>::second).second : _n);
+		int q = (int)lr.size();
+		int B = (_B == -1 ? max(1, n/int(sqrt(q))) : _B);
+
+		auto naive = [&](int idx) -> void {
+			snapshot();
+			const auto &[l, r] = lr[idx];
+			for(int i = l;i < r;i++)add_right(l, i);
+			f(idx);
+			rollback();
+		};
+
+		vector<vector<int>> index((n+B-1)/B);
+		index.reserve(q);
+		for(int i = 0;i < q;i++){
+			auto &[l, r] = lr[i];
+			if(l/B == r/B)naive(i);
+			else index[l/B].pb(i);
+		}
+
+		for(auto &v : index){
+			if(v.empty())continue;
+			sort(v.begin(), v.end(), [&](int i, int j){
+				const auto &[l_i, r_i] = lr[i];
+				const auto &[l_j, r_j] = lr[j];
+				return r_i < r_j;
+			});
+			int LMAX = 0;
+			for(auto &idx : v){
+				auto &[l, r] = lr[idx];
+				LMAX = max(LMAX, l);
+			}
+			reset();
+			int l = LMAX, r = LMAX;
+			for(auto &idx : v){
+				auto &[L, R] = lr[idx];
+				while(r < R)add_right(l, r++);
+				snapshot();
+				while(L < l)add_left(--l, r);
+				f(idx);
+				rollback();
+				l = LMAX;
+			}
+		}
+	}
+};
---
+++ documents/data_strucuture/rollbackmo.md
@@ -0,0 +1,12 @@
+---
+title: Rollback Mo
+documentation_of: ./../../data_structure/rollbackmo.hpp
+---
+
+# Rollback Mo
+
+- 削除操作のない Mo's Algorithm
+
+## 使い方
+
+実装の詳細は [ABC388-Gの提出](https://atcoder.jp/contests/abc388/submissions/61703817) 等を見るといい
