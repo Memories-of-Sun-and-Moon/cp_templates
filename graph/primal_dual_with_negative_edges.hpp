b439e726e0236daa767b7a2f94a1b292f0301655
Enceladus <univ.t1c2ha3@gmail.com>
2025-03-08 04:33:10 +0900

    primal_dual_with_negative_edge : バグ修正と、編の容量確認を追加

--- graph/primal_dual_with_negative_edges.hpp
+++ graph/primal_dual_with_negative_edges.hpp
@@ -4,6 +4,7 @@ struct primal_dual_with_negative_edges {
 	struct edge {
 		int to;
 		T cap, cost, rev;
+		T init_cap;
 	};
 	int V;
 	T infty;
@@ -11,16 +12,23 @@ struct primal_dual_with_negative_edges {
 	vector<T> dist;
 	vector<int> prevv, preve;
 
-	primal_dual_with_negative_edges(int _V) : V(_V), infty(numeric_limits<T>::max()/2) {
+	vector<int> from_idx, to_idx;
+	int edge_idx;
+
+	primal_dual_with_negative_edges(int _V) : V(_V), infty(numeric_limits<T>::max()/2), edge_idx(0) {
 		G.resize(V);
 		dist.resize(V);
 		prevv.resize(V);
 		preve.resize(V);
 	}
 
-	void add_edge(int from, int to, int cap, int cost) {
-		G[from].push_back((edge){to, cap, cost, (int)G[to].size()});
-		G[to].push_back((edge){from, 0, -cost, (int)G[from].size()-1});
+	int add_edge(int from, int to, T cap, T cost) {
+		G[from].push_back((edge){to, cap, cost, (int)G[to].size(), cap});
+		G[to].push_back((edge){from, 0, -cost, (int)G[from].size()-1, 0});
+		from_idx.emplace_back(from);
+		to_idx.emplace_back((int)G[from].size()-1);
+		
+		return edge_idx++;
 	}
 
 	pair<bool, T> min_cost_flow(int s, int t, T f) {
@@ -62,5 +70,9 @@ struct primal_dual_with_negative_edges {
 			}
 		}
 		return make_pair(true, res);
+	}
+
+	T get_flow(int idx){
+		return G[from_idx[idx]][to_idx[idx]].init_cap - G[from_idx[idx]][to_idx[idx]].cap;
 	}
 };
