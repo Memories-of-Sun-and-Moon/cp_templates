b4509f7197869d86db2a5464293f803d6925eba6
Enceladus <univ.t1c2ha3@gmail.com>
2025-03-09 13:19:51 +0900

    add assert

--- data_structure/binary_indexed_tree.hpp
+++ data_structure/binary_indexed_tree.hpp
@@ -5,12 +5,14 @@ template<typename T>struct binary_indexed_tree {
 	binary_indexed_tree(int n_) : n(n_ + 1), BIT(n, 0) {}
 
 	void add(int i, T x){
+		assert(1 <= i && i <= n);
 		for(int idx = i;idx < n;idx += (idx & -idx)){
 			BIT[idx] += x;
 		}
 	}
-
+	
 	T sum(int i) {
+		assert(1 <= i && i <= n);
 		T ret = 0;
 		for(int idx = i;idx > 0;idx -= (idx & -idx)){
 			ret += BIT[idx];
